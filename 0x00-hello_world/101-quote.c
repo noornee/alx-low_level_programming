cd02f22c0613d71d6f8c482a32d19a52e4ec09a0
noornee <kendrickbon11@gmail.com>
2022-03-10 11:18:52 +0100

    betty coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main() {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar,\
+2015-10-19\n");
+	return (1);
 }
