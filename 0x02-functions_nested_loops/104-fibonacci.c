b3258bb32483d835d1bba78da5984eaa706bb7b3
noornee <kendrickbon11@gmail.com>
2022-03-16 02:59:53 +0100

    fib-2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the first 98 fibonacci numbers starting from 1 and 2
+ * whose values do not exceed 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int n1, n2, nth, count;
+
+	n1 = 1;
+	n2 = 1;
+	count = 0;
+
+	while (count < 98)
+	{
+		nth = n1 + n2;
+		n1 = n2;
+		n2 = nth;
+		count += 1;
+		if (count != 98)
+			printf("%lu, ", n1);
+		else
+			printf("%lu\n", n1);
+
+	}
+
+	return (0);
+}
