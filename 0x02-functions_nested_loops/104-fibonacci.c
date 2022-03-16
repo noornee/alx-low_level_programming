7f2f442d631366ab02b741ed9ee33bb628d1b384
noornee <kendrickbon11@gmail.com>
2022-03-16 03:09:35 +0100

    welp, im givin up on this

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 	n2 = 1;
 	count = 0;
 
-	while (count < 90)
+	while (count < 98)
 	{
 		nth = n1 + n2;
 		n1 = n2;
 		n2 = nth;
 		count += 1;
-		if (count != 90)
+		if (count != 98)
 			printf("%lu, ", n1);
 		else
 			printf("%lu\n", n1);
