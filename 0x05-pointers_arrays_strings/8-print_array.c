7c6997f62663b043e10622549802615e201bbdfe
noornee <kendrickbon11@gmail.com>
2022-03-21 20:45:22 +0100

    edge case

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * print_array - prints elements of an array of integers
  * @a: a pointer to an array of type int
@@ -8,6 +10,9 @@
 void print_array(int *a, int n)
 {
 	int i;
+
+	if (n <= 0)
+		_putchar('\n');
 
 	for (i = 0; i < n; i++)
 	{
