5c632e7722d5e2257be0a38ab34277cf54d591f9
noornee <kendrickbon11@gmail.com>
2022-03-22 22:44:10 +0100

    figured the ish

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,25 @@
  * @s: a pointer to char
  * Return: void.
  */
+void print_rev(char *s);
+int main(void)
+{
+    char *str;
+
+    str = "holberton!";
+    print_rev(str);
+    return (0);
+}
+
 void print_rev(char *s)
 {
 	int i, len;
 
-	len = strlen(s);
+	len = strlen(s) - 1;
 
 	for (i = len; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
