dbbcd9bfcbcb2590ee8a23f65b7be990fe6fb7a8
noornee <kendrickbon11@gmail.com>
2022-03-24 16:46:47 +0100

    fix ? :/

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,10 @@ int _strcmp(char *s1, char *s2)
 	int ascii_s1 = 0; /* gets the ascii character of s1 */
 	int ascii_s2 = 0;
 
-	for (i = 0; i < strlen(s1); i++)
-		if (s1[i] != '\0')
-			break;
+	for (i = 0; i != '\0'; i++)
 		ascii_s1 += s1[i];
 
-	for (j = 0; j < strlen(s2); j++)
-		if (s2[j] != '\0')
-			break;
+	for (j = 0; j != '\0'; j++)
 		ascii_s2 += s2[j];
 
 	if (ascii_s1 == ascii_s2)
