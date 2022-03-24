8ddc4b9bd0f6f91cfc27dc8e9c4e09aee38288a7
noornee <kendrickbon11@gmail.com>
2022-03-24 16:55:45 +0100

    clean

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,3 @@
-#include <string.h>
-
 /**
  * _strcmp - compares two strings
  * @s1: 1st string to be compared
@@ -14,11 +12,13 @@ int _strcmp(char *s1, char *s2)
 	int ascii_s1 = 0; /* gets the ascii character of s1 */
 	int ascii_s2 = 0;
 
-	for (i = 0; i != '\0'; i++)
-		ascii_s1 += s1[i];
+	for (i = 0; s1[i] != '\0'; i++)
+		break;
+	ascii_s1 += s1[i];
 
-	for (j = 0; j != '\0'; j++)
-		ascii_s2 += s2[j];
+	for (j = 0; s2[j] != '\0'; j++)
+		break;
+	ascii_s2 += s2[j];
 
 	if (ascii_s1 == ascii_s2)
 		return (0);
