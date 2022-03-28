54ea9396fcd47baada3b18c174b5214bb7a9ee74
noornee <kendrickbon11@gmail.com>
2022-03-28 21:19:42 +0100

    mandatory tasks

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+/**
+ * _strstr - locates the first occurence substring.
+ * @haystack: string to be scanned.
+ * @needle: string to be searched within @haystack
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != *needle && *haystack != '\0')
+	{
+		haystack++;
+	}
+	printf("%c", haystack);
+	if (*haystack == *needle)
+		return (haystack);
+	return ('\0');
+
+}
