32488fcc446249d00c652e27a7f7062b8eb44ae3
noornee <kendrickbon11@gmail.com>
2022-04-01 15:53:51 +0100

    fixes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,9 @@
 /**
  * main - prints the number of arguments passed
  * @argc: argument count
- * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, mul;
+	int mul;
 
 	if (argc == 3)
 	{
