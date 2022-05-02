8e447a4a30889c95261f7e0239023b4d370c1a1b
noornee <kendrickbon11@gmail.com>
2022-05-02 23:34:56 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, nletters, rwr;
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
