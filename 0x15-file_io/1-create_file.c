4cf2ea3a52a3858aa6242a84768a4d1630b0316f
noornee <kendrickbon11@gmail.com>
2022-05-02 23:32:34 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * create_file - creates a file.
  * @filename: name of the file to create.
@@ -6,5 +8,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd, nletters, rwr;
 
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
+
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: name of the file.
@@ -6,5 +7,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int fd, nletters, rwr;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
+
